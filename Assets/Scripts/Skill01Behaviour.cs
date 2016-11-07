2b703452fec391b3365310e0eeb33bf16a44bc83
U-DESKTOP-TKO3H37\Abject <maxime.radomski@epitech.eu>
2016-11-07 23:50:53 +0900

    Bombs and Skills OK

---
+++ Assets/Scripts/Skill01Behaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skill01Behaviour : MonoBehaviour
+{
+    public float shotDelay = 1.0f;
+    public int skillNumber = -1;
+    public Sprite full;
+    public Sprite empty;
+
+    private bool readyToShoot = true;
+    private int currentSkillNumber;
+
+    // Use this for initialization
+    void Start ()
+    {
+        currentSkillNumber = skillNumber;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.O) && readyToShoot && skillNumber != -1 && currentSkillNumber > 0)
+        {
+            GameObject.Find("HUDSkill0" + currentSkillNumber).GetComponent<SpriteRenderer>().sprite = empty;
+            readyToShoot = false;
+            Invoke("ResetReadyToShoot", shotDelay);
+            --currentSkillNumber;
+        }
+    }
+
+    void ResetReadyToShoot()
+    {
+        readyToShoot = true;
+    }
+}
